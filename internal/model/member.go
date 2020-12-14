e6294d1969d31ade7dd45c8e43d50439faecf58a
firllyw <fwahyudi@gmail.com>
2020-12-14 23:11:07 +0700

    feat: full cycle member endpoint

--- internal/model/member.go
+++ internal/model/member.go
@@ -10,12 +10,14 @@ type Member struct {
 	Address string
 }
 
-// MemberService represents Member use case contract
-type MemberService interface {
-	Index(ctx context.Context, query map[string]interface{}, page, size int) ([]Member, map[string]int, error)
+// Service represents Member use case contract
+type Service interface {
+	Index(ctx context.Context, query map[string]interface{}, page, size int) ([]Member, error)
+	Insert(ctx context.Context, data Member) (Member, error)
 }
 
-// MemberRepo represents member repo contract
-type MemberRepo interface {
+// Repo represents member repo contract
+type Repo interface {
 	Index(ctx context.Context, query map[string]interface{}, page, size int) ([]Member, error)
+	Insert(ctx context.Context, data Member) (Member, error)
 }
--- internal/services/member.go
+++ internal/services/member.go
@@ -1,15 +1,26 @@
 package services
 
 import (
-	"clean-arch/model"
+	"context"
+
+	member "github.com/goclean/internal/model"
 )
 
 type memberService struct {
-	repo model.MemberRepo
+	repo member.Repo
 }
 
-func NewMemberService(repo model.MemberRepo) model.MemberService {
-	return &NewMemberService{
+// NewMemberService initiate new Member Service based on Contract
+func NewMemberService(repo member.Repo) member.Service {
+	return &memberService{
 		repo: repo,
 	}
+}
+
+func (m *memberService) Index(ctx context.Context, query map[string]interface{}, page, size int) (members []member.Member, err error) {
+	return m.repo.Index(ctx, query, page, size)
+}
+
+func (m *memberService) Insert(ctx context.Context, data member.Member) (member.Member, error) {
+	return m.repo.Insert(ctx, data)
 }
